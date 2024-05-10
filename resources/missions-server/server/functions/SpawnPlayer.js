14d3454a62d6ac68ebaab545c91c8a586b07d6ce
AlexSavr <as@sava.team>
2024-05-10 18:28:30 +0300

    Update SpawnPlayer.js
    fix typo error

--- resources/missions-server/server/functions/SpawnPlayer.js
+++ resources/missions-server/server/functions/SpawnPlayer.js
@@ -3,6 +3,6 @@ function SpawnPlayer(client, coords) {
         GlobalConfig.NewCharacter.fSpawnY, 
         GlobalConfig.NewCharacter.fSpawnZ];    
     triggerNetworkEvent("setWantedLevel", client, 0, false);
-    spawnPlayer(client, coords || defaultCoords, 0, PlayerData[client.index].iSkinID || GlobalConfig.NewCharacter.iiSkinID);
+    spawnPlayer(client, coords || defaultCoords, 0, PlayerData[client.index].iSkinID || GlobalConfig.NewCharacter.iSkinID);
 	fadeCamera(client, true);
 }
